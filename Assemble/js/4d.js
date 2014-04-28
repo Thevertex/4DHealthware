5b95dfe73babc726285a60cf85fa0cb02e76ebe3
Mare Greyling <mare@coffeeforless.com>
2014-04-28 19:51:36 +0200

    Updates to responsive coming soon

--- Assemble/js/4d.js
+++ Assemble/js/4d.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $(window).resize(function(){
         $('.main-banner').height($('.logo-banner img').height()-10);
+        $('.row').css('margin-top',$('body').height()-$('.row').height()-($('body').height()*10/100)+'px');
     });
     $('.main-banner').height($('.logo-banner img').height()-10);
+    $('.row').css('margin-top',$('body').height()-$('.row').height()-($('body').height()*10/100)+'px');
 });
