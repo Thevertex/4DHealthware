bf2038e8d88f96d5469cd276397cd5bfb105335d
Mare Greyling <mare@coffeeforless.com>
2014-04-28 12:38:01 +0200

    Updates

---
+++ Assemble/js/4d.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(window).resize(function(){
+        $('.main-banner').height($('.logo-banner img').height()-10);
+    });
+    $('.main-banner').height($('.logo-banner img').height()-10);
+});
